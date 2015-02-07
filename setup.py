2eef03fa3c65cec8f246f460ce2e43f435c1346a
Radek Podgorny <radek@podgorny.cz>
2015-02-07 23:33:03 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+from omnirun import __version__
+
+setup(
+	name = 'omnirun',
+	version = __version__,
+	description = 'run command on many hosts',
+	url = 'https://github.com/rpodgorny/omnirun',
+	author = 'Radek Podgorny',
+	author_email = 'radek@podgorny.cz',
+	license = 'GPL',
+	packages = find_packages(),
+	entry_points = {
+		'console_scripts': ['omnirun=omnirun:main', ],
+	},
+)
