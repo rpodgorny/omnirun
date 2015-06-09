0375fe11ae06ad7c29affbb6f2517ada2e2b10a7
Radek Podgorny <radek@podgorny.cz>
2015-06-09 11:05:26 +0200

    improve host expansion

--- omnirun/main.py
+++ omnirun/main.py
@@ -90,16 +90,26 @@ def expand_host(s):
 	if not '[' in s or not '-' in s or not ']' in s: return [s, ]
 
 	pre = s.split('[')[0]
+	in_ = s.split('[')[1].split(']')[0]
 	post = s.split(']')[1]
 
-	from_ = s.split('[')[1].split('-')[0]
-	to_ = s.split(']')[0].split('-')[1]
-	from_ = int(from_)
-	to_ = int(to_)
-
 	ret = []
-	for i in range(from_, to_ + 1):
-		ret.append('%s%s%s' % (pre, i, post))
+
+	for i in in_.split(','):
+		i = i.strip()
+		if not i: continue
+
+		if '-' in i:
+			from_, to_ = i.split('-', 1)
+			from_ = int(from_)
+			to_ = int(to_)
+
+			for j in range(from_, to_ + 1):
+				ret.append('%s%s%s' % (pre, j, post))
+			#endfor
+		else:
+			ret.append('%s%s%s' % (pre, i, post))
+		#endif
 	#endfor
 
 	return ret
