685aae7c726f6ca38422d6b6c6d081e5ed8619bc
Radek Podgorny <radek@podgorny.cz>
2016-11-13 00:40:29 +0100

    more tests

--- tests/test.py
+++ tests/test.py
@@ -1,10 +1,14 @@
 import unittest
-
 import omnirun.main
 
+
 class MyTestCase(unittest.TestCase):
-	def test_test(self):
-		omnirun.main.expand_host('192.168.100.[1-100]')
-	#enddef
-#endclass
+	def test_expand_host(self):
+		self.assertEqual(len(omnirun.main.expand_host('192.168.100.[1-100]')), 100)
+
+	def test_host_to_user_pass_host_port(self):
+		self.assertEqual(omnirun.main.host_to_user_pass_host_port('user:pass@host:4444'), ('user', 'pass', 'host', 4444))
+		self.assertEqual(omnirun.main.host_to_user_pass_host_port('user@host:4444'), ('user', None, 'host', 4444))
+		self.assertEqual(omnirun.main.host_to_user_pass_host_port('host:4444'), (None, None, 'host', 4444))
+		self.assertEqual(omnirun.main.host_to_user_pass_host_port('host'), (None, None, 'host', None))
 
