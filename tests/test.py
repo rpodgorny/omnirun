9f23518d89ff4f6dbf96db46e7c45a3ae571b48b
Radek Podgorny <radek@podgorny.cz>
2016-11-14 01:34:46 +0100

    updated tests

--- tests/test.py
+++ tests/test.py
@@ -6,9 +6,9 @@ class MyTestCase(unittest.TestCase):
 	def test_expand_host(self):
 		self.assertEqual(len(omnirun.main.expand_host('192.168.100.[1-100]')), 100)
 
-	def test_host_to_user_pass_host_port(self):
-		self.assertEqual(omnirun.main.host_to_user_pass_host_port('user:pass@host:4444'), ('user', 'pass', 'host', 4444))
-		self.assertEqual(omnirun.main.host_to_user_pass_host_port('user@host:4444'), ('user', None, 'host', 4444))
-		self.assertEqual(omnirun.main.host_to_user_pass_host_port('host:4444'), (None, None, 'host', 4444))
-		self.assertEqual(omnirun.main.host_to_user_pass_host_port('host'), (None, None, 'host', None))
+	def test_hostspec_to_user_pass_host_port(self):
+		self.assertEqual(omnirun.main.hostspec_to_user_pass_host_port('user:pass@host:4444'), ('user', 'pass', 'host', 4444))
+		self.assertEqual(omnirun.main.hostspec_to_user_pass_host_port('user@host:4444'), ('user', None, 'host', 4444))
+		self.assertEqual(omnirun.main.hostspec_to_user_pass_host_port('host:4444'), (None, None, 'host', 4444))
+		self.assertEqual(omnirun.main.hostspec_to_user_pass_host_port('host'), (None, None, 'host', None))
 
